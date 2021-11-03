e5e9f31502b80d6ea485c444133aa50d9822a719
DriCro6663 <dri.cro.6663@gmail.com>
2021-11-04 08:20:26 +0900

    first commit

--- proxy/99-del-proxy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Delete proxy, https-proxy and registry setting for npm
-
-call npm -g config delete proxy
-call npm -g config delete https-proxy
-call npm -g config delete registry
-
-echo Completed...
